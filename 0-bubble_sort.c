4827f842768b4bb3beb9c8e33f4a55097651641c
AngieCastano1634 <1634@holbertonschool.com>
2020-06-15 11:21:06 -0500

    [task #0][prints same as the example main]

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ * bubble_sort - bubble sort algorithm
+ * @array: array to sort
+ * @size: array's size
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0;
+	size_t temp = 0;
+	int validations = 0;
+
+	for (i = 0; array[i]; i++)
+	{
+		temp = 0;
+
+		if (i < size - 1 && array[i + 1] < array[i])
+		{
+			temp = array[i];
+			array[i] = array[i + 1];
+			array[i + 1] = temp;
+			print_array(array, size);
+			validations++;
+		}
+		if (i == size - 1)
+		{
+			i = -1;
+			if (validations == 0)
+			{
+				return;
+			}
+			validations = 0;
+		}
+	}
+}
