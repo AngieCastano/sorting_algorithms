6bd828ac7481156eea0d1f5eeea9021c05d98cc3
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-06-17 03:01:13 +0000

    [add]

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,8 +1,8 @@
 #include "sort.h"
 /**
- * main - Entry point
- * @array:
- * @size:
+ * shell_sort - sort array in shell format
+ * @array: array to sort
+ * @size: array size
  * Return: nothing
  */
 void shell_sort(int *array, size_t size)
@@ -14,20 +14,21 @@ void shell_sort(int *array, size_t size)
 		return;
 	for (gap = 1; gap < size / 3; gap = gap * 3 + 1)
 		;
+
 	for (; gap > 0; gap = gap / 3)
+	{
+		impri = 0;
+		for (j = gap; j < size; j += 1)
 		{
-			impri = 0;
-			for (j = gap; j < size; j += 1)
+			temp = array[j];
+			for (k = j; k >= gap && array[k - gap] > temp; k -= gap)
 			{
-				temp = array[j];
-				for (k = j; k >= gap && array[k - gap] > temp; k -= gap)
-				{
-					array[k] = array[k - gap];
-				}
-				array[k] = temp;
-				impri = 1;
+				array[k] = array[k - gap];
 			}
-			if (impri == 1)
-				print_array(array, size);
+			array[k] = temp;
+			impri = 1;
 		}
+		if (impri == 1)
+			print_array(array, size);
+	}
 }
